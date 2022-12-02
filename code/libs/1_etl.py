d51024ff547e6bb473873de1d454e076a54eddf6
Gil Estéfano Rodríguez Rivera <ge.rodriguezrivera@ugto.mx>
2022-12-02 03:47:36 -0600

    Update 1_etl.py

--- code/libs/1_etl.py
+++ code/libs/1_etl.py
@@ -10,7 +10,7 @@ class DataTreatment:
         pdn_system (str): The PDN system you wish to work with:
                             's1', 's2', 's3p' or 's3s'
     '''
-    # TODO: ADD A FUNCTIONALITY TO ALSO READ URLS
+    # TODO: ADD A FUNCTIONALITY TO ALSO WORKS WITH URLS
 
     # Define the system
     if type(pdn_system)==str:
@@ -26,89 +26,134 @@ class DataTreatment:
       self.root_path = root_path
     else:
       raise TypeError('Error on __init__ method:   Only string type is allowed for root_path')
+    
+    self.last_data = pd.DataFrame({})
+    self.tables = dict()
 
     return None
-  
-  # PRIVATE METHODS
-  def _getPaths(self, main_path:str) -> list:
-    return glob(main_path, '*', recursive=False)
 
-  def _readJson(self, path:str) -> dict:
+  # ----------------------------------------------------------------------------
+  # ----------------------------------------------------------------------------
+  # ----------------------------------------------------------------------------
+  # PRIVATE METHODS
+  # ----------------------------------------------------------------------------
+  # ----------------------------------------------------------------------------
+  # ----------------------------------------------------------------------------
+  def __getPaths(self, main_path:str) -> list:
+    return glob(main_path+'/*', recursive=False)
+
+  def __readJson(self, path:str) -> dict:
     with open(path, 'r') as f:
       dictionary = json.load(f)
     return dictionary
   
-  def _normalizeStateName(self, string:str) -> str:
+  def __normalizeStateName(self, string:str) -> str:
     return string.replace('SESNA', '').replace('_','').replace(' ', '').lower()
+
+  def __execTableMethod(self, method:str, input:str, output:str) ->  None:
+    self.tables[output] = eval("""self._{0}__{1}({2})""".format(__class__.__name__, method, input))
+    self.last_data = self.tables[output].copy()
+    return None
   
-  def _flattenDictionary(self, dictionary:dict, separator:str='_', prefix:str='') -> dict:
+  def __flattenDictionary(self, dictionary:dict,
+                         separator:str='_', prefix:str='') -> dict:
     # Source: https://www.geeksforgeeks.org/python-convert-nested-dictionary-into-flattened-dictionary/
-    return { prefix + separator + k if prefix else k : v
+    return { prefix + separator + str(k) if prefix else str(k) : v
             for kk, vv in dictionary.items()
-            for k, v in self._flattenDictionary(vv, separator, kk).items()
+            for k, v in self.__flattenDictionary(vv, separator, str(kk)).items()
           } if isinstance(dictionary, dict) else { prefix : dictionary }
   
-  def _flattenStatesData(self, state_paths:list) -> pd.DataFrame:
+  def __flattenStatesData(self, state_paths:list) -> pd.DataFrame:
     # TODO: REFACTOR AND MODULARIZE THIS CODE
-    normalized_data = pd.DataFrame({})
+    flattened_data = pd.DataFrame({})
     
     for state_path in state_paths:
       # Get the state name and normalize it
-      state = self._normalizeStateName(state_path.split('/')[-1])
+      state = self.__normalizeStateName(state_path.split('/')[-1])
 
       # Get every data file on the state
-      single_files = self._getPaths(state_path)
+      single_files = self.__getPaths(state_path)
       state_list = []
       for single_file in single_files:
+
         # Read the json as a dictionary
-        dictionary_file = self._readJson(single_file)
+        dictionary_file = self.__readJson(single_file)
 
-        # Flatten the dictionary
-        flatten_dictionary = self._flattenDictionary(dictionary_file)
-
-        # Add the flattened dictionary to the list of every file in the state
-        state_list.append(flatten_dictionary)
+        # Flatten the dictionary and add it to the whole states' list
+        state_list += [self.__flattenDictionary(item) for item in dictionary_file]
       
       # Normalize the state data
       state_data = pd.DataFrame(state_list)
       state_data['estado'] = state
-      normalized_data.append(state_data, ignore_index=True)
+      flattened_data = flattened_data.append(state_data, ignore_index=True)
 
-      return normalized_data
-  
-  def _tokenizeData(self):
-    pass
-  
-  def _missingTreatment(self):
-    pass
+    return flattened_data
 
+  def __normalizeData(self, input_name:str) -> pd.DataFrame:
+    # Source: https://stackoverflow.com/questions/35491274/split-a-pandas-column-of-lists-into-multiple-columns
+    input_table = self.tables[input_name[0]]
+
+    output_table = pd.DataFrame({})
+    for column in input_table.columns:
+      # Check if any element in the column is a list
+      if any(isinstance(item, list) for item in input_table[column].to_list()):
+        # Extend the list in a single column into multiple columns with a single element
+        df_dummy = pd.DataFrame([pd.Series(x) for x in input_table[column]])
+        df_dummy.columns = [column+'_{}'.format(i+1) for i in df_dummy.columns]
+
+        # If there are nested dictionaries inside the elements, it flattens them
+        df_dummy_list = df_dummy.to_dict('records')
+        for i in range(len(df_dummy_list)):
+          df_dummy_list[i] = self.__flattenDictionary(df_dummy_list[i])
+        df_dummy = pd.DataFrame(df_dummy_list)
+
+
+      # If there's no list in the dataframe column, read the column
+      else:
+        df_dummy = input_table[column]
+
+      # Concat the treated columns into the output
+      output_table = pd.concat([output_table, df_dummy], axis=1)
+    return output_table
+
+  def __missingTreatment(self, input_name:str) -> pd.DataFrame:
+    # TODO: INCLUDE PERSONALIZED MISSING TREATMENT WITH MORE INTERPRETABILITY
+    input_table = self.tables[input_name[0]]
+    return input_table.fillna(0)
+
+  # ----------------------------------------------------------------------------
+  # ----------------------------------------------------------------------------
+  # ----------------------------------------------------------------------------
   # PUBLIC METHODS
-  def extractData(self) -> None:
+  # ----------------------------------------------------------------------------
+  # ----------------------------------------------------------------------------
+  # ----------------------------------------------------------------------------
+  def extractData(self, output_table:str='flattened_data') -> None:
     # The main path of the PDN system
     system_root_path = os.path.join(self.root_path, self.pdn_system)
 
     # The list of every state on the PDN system
-    state_paths = self._getPaths(system_root_path)
+    state_paths = self.__getPaths(system_root_path)
 
     # Flatten all the states' data and append it into a single dataframe
-    self.flattened_data = self._flattenStatesData(state_paths)
-    self.last_data = self.flattened_data
+    self.__execTableMethod(method='flattenStatesData',
+                          input=state_paths, output=output_table)
 
     return None
   
-  def normalizeData(self) -> None:
+  def normalizeData(self, input_table:str='last_data',
+                    output_table:str='normalized_data') -> None:
     # This method extends every element  with a list within the dataframe 
     # into multiple columns in the same dataframe
-    self.normalized_data = self._normalizeData()
-    self.last_data = self.normalized_data
-
+    self.__execTableMethod(method='normalizeData',
+                          input=[input_table], output=output_table)
     return None
 
-  def missingData(self) -> None:
+  def missingData(self, input_table:str='last_data',
+                  output_table:str='missing_data') -> None:
     # This method turns all missing data into a value
-    self.no_missing_data = self._missingTreatment()
-    self.last_data = self.no_missing_data
-
+    self.__execTableMethod(method='missingTreatment',
+                          input=[input_table], output=output_table)
     return None
  
 class FeatureEngineering(DataTreatment):
